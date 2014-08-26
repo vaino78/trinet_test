984a48193c9d0d917ecd20faaa207fe7a7661000
Ivan Danilov <vaino78@yandex.ru>
2014-08-27 00:36:18 +0400

    Реализация бизнес-логики на стороне БД

--- classes/mysql/section_manage.php
+++ classes/mysql/section_manage.php
@@ -44,21 +44,89 @@ class CTTSectionManage extends ATTSectionManage
 	protected function getSections($iblock_id)
 	{
 		$q = $this->getDb()->Query(sprintf(
-
+			(
+				'SELECT B.`ID` '
+				. ' FROM `b_iblock_section` A '
+				. ' LEFT JOIN `b_iblock_section` B ON('
+				. '      A.`IBLOCK_ID`=B.`IBLOCK_ID` '
+				. '   && B.`LEFT_MARGIN` >= A.`LEFT_MARGIN` '
+				. '   && B.`RIGHT_MARGIN` <= A.`RIGHT_MARGIN`)'
+				. ' WHERE    A.`IBLOCK_ID`=%u '
+				. '       && A.`ID` IN(%s)'
+				. ' ORDER BY B.`LEFT_MARGIN`'
+			),
+			$iblock_id,
+			implode(',', array_map('intval', $this->parents))
 		));
+
+		while($d = $q->Fetch())
+			$this->sections[] = $d['ID'];
+
+		return $this->sections;
 	}
 
 	protected function getProducts($iblock_id)
-	{}
+	{
+		$q = $this->getDb()->Query(sprintf(
+			(
+				'SELECT E.`ID` '
+				. ' FROM       `b_iblock_section_element` SE '
+				. ' INNER JOIN `b_iblock_element`         E  ON(SE.`IBLOCK_ELEMENT_ID`=E.`ID`) '
+				. ' INNER JOIN `b_catalog_product`        P  ON(E.`ID`=P.`ID`) '
+				. ' INNER JOIN `b_catalog_price`          PP ON(P.`ID`=PP.`PRODUCT_ID`) '
+				. ' WHERE E.`IBLOCK_ID`=%u && SE.`IBLOCK_SECTION_ID` IN(%s) '
+				. ' GROUP BY E.`ID`'
+			),
+			$iblock_id,
+			implode(',', $this->sections)
+		));
+
+		while($d = $q->Fetch())
+			$this->products[] = $d['ID'];
+
+		return $this->products;
+	}
 
 	protected function processUpdate()
-	{}
+	{
+		$q = $this->getDb()->Query(sprintf(
+			'UPDATE `b_catalog_price` SET `PRICE` = (`PRICE` + (`PRICE` * %.02f)) WHERE `PRODUCT_ID` IN(%s)',
+			($this->value / 100),
+			implode(',', $this->products)
+		));
+
+		return $this->affected = $this->getDb()->AffectedRowsCount();
+	}
 
 	protected function logSections()
-	{}
+	{
+		$this->getDb()->Query(sprintf(
+			'INSERT INTO `%s`(`log_id`, `section_id`) VALUES %s',
+			self::TABLE_LOG_SECTIONS,
+			implode(', ', array_map(
+				function($section_id) use($this->id as $log_id)
+				{
+					return sprintf('(%u, %u)', $log_id, $section_id);
+				},
+				$this->sections
+			))
+		));
+	}
 
 	protected function logProducts()
-	{}
+	{
+		$this->getDb()->Query(sprintf(
+			'INSERT INTO `%s`(`log_id`, `product_id`) VALUES %s',
+			self::TABLE_LOG_PRODUCTS,
+			implode(', ', array_map(
+				function($product_id) use($this->id as $log_id)
+				{
+					return sprintf('(%u, %u)', $log_id, $product_id);
+				},
+				$this->products
+			))
+		));
+	}
 
 	protected function getDb()
 	{
