b8f0ae8d085713333a19b6f57280d019fbea5c9b
Ivan Danilov <vaino78@yandex.ru>
2014-08-29 01:41:41 +0400

    Работа с настройками

--- classes/general/price_manager.php
+++ classes/general/price_manager.php
@@ -4,11 +4,11 @@ class CTTPriceManager implements ITTModuleSettings
 {
 	protected static $error;
 
-	private static $opts_int = array(
+	protected static $opts_int = array(
 		'catalog_iblock_id'
 	);
 
-	private static $opts_req = array(); 
+	protected static $opts_req = array(); 
 
 	public static function manageBySection(array $parentSection, $value, $settings = 0, $userID = false)
 	{
@@ -25,28 +25,81 @@ class CTTPriceManager implements ITTModuleSettings
 		return $result;
 	}
 
+
+	public static function getCatalogList()
+	{
+		if(!CModule::IncludeModule('catalog'))
+			return false;
+
+		$q = CCatalog::GetList();
+		$result = array();
+		while($d = $q->Fetch())
+			$result[$d['IBLOCK_ID']] = $d['NAME'];
+
+		return $result;
+	}
+
+
+	public static function saveSettings(&$settings)
+	{
+		if(!static::validateSettings($settings))
+			return false;
+
+		static::setOption('catalog_iblock_id', $settings['catalog_iblock_id']);
+
+		return true;
+	}
+
+
+	public static function validateSettings(&$settings)
+	{
+		try
+		{
+			if(empty($settings['catalog_iblock_id']))
+				throw new Exception(GetMessage('VALIDATE_CATALOG_IBLOCK_EMPTY'));
+
+			if(!CModule::IncludeModule('catalog') || !CCatalog::GetByID($settings['catalog_iblock_id']))
+				throw new Exception(GetMessage('VALIDATE_IBLOCK_IS_NOT_CATALOG'));
+
+		}
+		catch(Exception $e)
+		{
+			static::$error = $e->getMessage();
+			return false;
+		}
+
+		return true;
+	}
+
+
+	public static function getError()
+	{
+		return static::$error;
+	}
+
+
 	public static function getOption($name)
 	{
-		if(in_array($name, self::$opts_int))
-			return COption::GetOptionInt(self::MODULE_ID, $name);
-		return COption::GetOptionString(self::MODULE_ID, $name);
+		if(in_array($name, static::$opts_int))
+			return COption::GetOptionInt(static::MODULE_ID, $name);
+		return COption::GetOptionString(static::MODULE_ID, $name);
 	}
 
 	public static function setOption($name, $value)
 	{
-		if(in_array($name, self::$opts_int))
-			return COption::SetOptionInt(self::MODULE_ID, $name, $value);
-		return COption::SetOptionString(self::MODULE_ID, $name, $value);
+		if(in_array($name, static::$opts_int))
+			return COption::SetOptionInt(static::MODULE_ID, $name, $value);
+		return COption::SetOptionString(static::MODULE_ID, $name, $value);
 	}
 
 	public static function isOptionRequired($name)
 	{
-		return in_array($name, self::$opts_req);
+		return in_array($name, static::$opts_req);
 	}
 	
 	public static function isOptionsInt($name)
 	{
-		return in_array($name, self::$opts_int);
+		return in_array($name, static::$opts_int);
 	}
 
 }
--- options.php
+++ options.php
@@ -1,3 +1,62 @@
 <?php
 
+if(!CModule::IncludeModule('trinet_test'))
+	die('Module is not installed');
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && check_bitrix_sessid())
+{
+	$result = CTTPriceManager::saveSettings($_POST);
+
+
+}
+
+$tabCtrl = new CAdminTabControl(
+	'tabControl',
+	array(
+		'DIV' => 'tt_tab1',
+		'TAB' => GetMessage('OPTS_GENERAL_TAB_NAME'),
+		'TITLE' => GetMessage('OPTS_GENERAL_TAB_TITLE')
+	)
+);
+
+$tabCtrl->Begin();
 ?>
+<form method="POST" action="<?echo $APPLICATION->GetCurPage()?>?mid=<?=htmlspecialchars($mid)?>&amp;lang=<?echo LANG?>">
+<?=bitrix_sessid_post();?>
+
+<?php
+$tabControl->BeginNextTab();
+?>
+	<tr>
+		<td width="50%" valign="top"><?=GetMessage('OPTS_GENERAL_CATALOG_IBLOCK_ID')?> <span style="color:red;">*</span></td>
+		<td><select name="catalog_iblock_id">
+			<option value="0"><?=GetMessage('OPTS_GENERAL_CATALOG_IBLOCK_NOT_SELECTED')?></option>
+			<?php
+			$catalogs = CTTPriceManager::getCatalogList();
+			$selected = CTTPriceManager::getOption('catalog_iblock_id');
+			foreach($catalogs as $iblock_id => $name)
+			{
+				?><option value="<?=$iblock_id?>"<?if($iblock_id == $selected):?> selected="selected"<?endif?>><?=$name?></option><?php
+			}
+			?>
+		</select></td>
+	</tr>
+<?php
+$tabControl->Buttons();?>
+
+<script language="JavaScript">
+function confirmRestoreDefaults()
+{
+	return confirm('<?echo AddSlashes(GetMessage("MAIN_HINT_RESTORE_DEFAULTS_WARNING"))?>');
+}
+</script>
+<input type="submit" name="Update" value="<?echo GetMessage("MAIN_SAVE")?>">
+<input type="hidden" name="Update" value="Y">
+<input type="reset" name="reset" value="<?echo GetMessage("MAIN_RESET")?>">
+<!-- input type="submit" name="RestoreDefaults" title="<?echo GetMessage("MAIN_HINT_RESTORE_DEFAULTS")?>" OnClick="return confirmRestoreDefaults();" value="<?echo GetMessage("MAIN_RESTORE_DEFAULTS")?>" -->
+
+<?
+$tabControl->End();
+?>
+
+</form>
